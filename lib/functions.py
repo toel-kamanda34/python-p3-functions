b11006505b9e6c32d3dd8beaf469291742cc3287
toel-kamanda34 <toelokemwa@gmail.com>
2024-08-28 13:27:59 +0300

    define my own python functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
-
+    #pass
+    print("Hello, programmer!")
+print(greet_programmer())
 def greet(name):
-    pass
-
+    #pass
+    print(f"Hello, {name}!")
+print(greet("Toel"))
 def greet_with_default(name="programmer"):
-    pass
-
+    #pass
+    print(f"Hello, {name}!")
+print(greet_with_default())
 def add(num1, num2):
-    pass
-
+    #pass
+    return num1 + num2
+print(add(2,2))
 def halve(number):
-    pass
+    #pass
+    return number / 2
+print(halve(4))
+
+
+ 
+
+
+
+
+
+
